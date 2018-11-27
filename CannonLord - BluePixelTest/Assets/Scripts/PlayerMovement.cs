5b8913dccfe16e055ce22797911a8a705e82d696
Diego Gomes Barbosa <dgb.diego@gmail.com>
2018-11-27 16:19:36 -0200

    Scripts do player
    Criado Script de movimento do canhão e controle de disparo da arma.
    Testando colisão com inimigos

---
+++ CannonLord - BluePixelTest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public GameObject target;
+	public int attackAngle;
+
+	
+	public float speedMovement;
+	public Vector3 point;
+	public bool positive = true;
+
+	void Start () {
+
+		point = target.transform.localEulerAngles;
+		
+	}	
+	// Update is called once per frame
+	void FixedUpdate () {			
+
+		movement();
+		
+	}
+	
+	public void movement(){	
+
+		if (positive){
+			point.z += attackAngle * (speedMovement / 10f);
+		} else {
+			point.z -= attackAngle * (speedMovement / 10f);
+		}
+	
+		if (point.z < 90f -attackAngle){
+			positive = true;			
+		} else if (point.z > 90 + attackAngle){
+			positive = false;
+		}
+
+		target.transform.localEulerAngles = point;
+
+	}
+}
---
+++ CannonLord - BluePixelTest/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour {
+
+	public ParticleSystem particles;
+	public GameObject explosion;
+
+	void OnParticleCollision(GameObject other) {
+
+		GameObject exp = Instantiate (explosion, other.transform.position, other.transform.rotation) as GameObject;
+		Destroy(other);
+		Destroy(exp, 1.2f);		
+		
+	}
+
+	public void shoot(){
+
+		particles.Emit(1);
+	}
+
+
+}
