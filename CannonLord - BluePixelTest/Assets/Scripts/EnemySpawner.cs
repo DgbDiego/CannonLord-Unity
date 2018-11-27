ee29e7a6da7fe1a0eac731614db81a74ea45f91f
Diego Gomes Barbosa <dgb.diego@gmail.com>
2018-11-27 19:39:27 -0200

    Implementação grande
    EnemySpawner criado
    2 inimigos funcionando
    player pode destruir eles
    começando tela de início

---
+++ CannonLord - BluePixelTest/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject[] enemies;
+
+	public GameObject target;
+	Vector3 point;
+	public bool positive;
+	public float movementRange;
+	public float speedMovement;
+
+	float intTimer;
+	public float spawnFreq;
+
+	// Use this for initialization
+	void Start () {
+
+		intTimer = Time.time + spawnFreq;
+		point = target.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		movement();
+
+		if (intTimer < Time.time){
+			intTimer = Time.time + spawnFreq;
+			newEnemy();
+		}
+		
+	}
+
+	public void newEnemy(){
+
+		GameObject newEnemy = Instantiate(enemies[Random.Range(0,enemies.Length)], transform.position, transform.rotation) as GameObject;
+	}
+
+	public void movement(){	
+
+		if (positive){
+			point.x += movementRange * (speedMovement / 10f);
+		} else {
+			point.x -= movementRange * (speedMovement / 10f);
+		}
+	
+		if (point.x < -movementRange){
+			positive = true;			
+		} else if (point.x > movementRange){
+			positive = false;
+		}
+
+		target.transform.position = point;
+
+	}
+}
---
+++ CannonLord - BluePixelTest/Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySystem : MonoBehaviour {
+
+	public float speedForward;
+	public float speedSideward;
+	Vector3 direction;
+	Vector3 point;
+	public bool positive;
+
+
+	// Use this for initialization
+	void Start () {
+
+		point = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		movement();
+
+	}
+
+	public void movement(){	
+
+		if (positive){
+			point += new Vector3(speedSideward / 10f, speedForward / 10f, 0f );
+		} else {
+			point += new Vector3 ( - speedSideward / 10f, speedForward / 10f, 0f );
+		}
+
+		transform.position = point;
+
+	}
+
+
+private void OnTriggerEnter2D(Collider2D other) {
+
+		Debug.Log ("colidiu");
+
+		switch (other.gameObject.name){
+
+			case "Left" :
+				positive = true;
+				break;
+			case "Right" :
+				positive = false;
+				break;
+			case "End" :
+				Destroy(this.gameObject);
+				break;
+			default:
+				break;
+
+		}
+
+		
+	}
+}
